ab7989f4739bd9fbcda3ca233357289c893ea13b
monstermaria <42701428+monstermaria@users.noreply.github.com>
2020-08-25 10:09:21 +0200

    Added logging of error messages

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const peerOnOpen = (id) => {
     document.querySelector('.my-peer-id').innerHTML = id;
 }
 
+const peerOnError = (error) => {
+    console.log(error);
+}
+
 const myPeerId = location.hash.slice(1);
 console.log(myPeerId);
 
 let peer = new Peer(myPeerId, { host: 'glajan.com', port: 8443, path: '/myapp', secure: true});
 
 peer.on('open', peerOnOpen);
-
+peer.on('error', peerOnError);
 
