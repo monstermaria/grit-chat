70f15fbbb02a768657d6e15c81fd47489511b6c5
monstermaria <42701428+monstermaria@users.noreply.github.com>
2020-08-25 09:52:22 +0200

    Added peer id

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 // just a comment, to show i know how to push to git :)
 // and another one, just for fun...
 // blaha
+
+const peerOnOpen = (id) => {
+    document.querySelector('.my-peer-id').innerHTML = id;
+}
+
+const myPeerId = location.hash.slice(1);
+console.log(myPeerId);
+
+let peer = new Peer(myPeerId, { host: 'glajan.com', port: 8443, path: '/myapp', secure: true});
+
+peer.on('open', peerOnOpen);
+
+
